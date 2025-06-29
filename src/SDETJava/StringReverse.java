9537402490a5006a97114603eef685fbe1d41780
ketanr93 <ketanrathod0593@gmail.com>
2025-06-29 21:03:11 +0530

    Stringreverse and Arrayreversal added to SDET-java collection

---
+++ src/SDETJava/StringReverse.java
@@ -0,0 +1,22 @@
+package test_project;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str2 = "Marvel";
+        String str3 = "Thanos";
+        System.out.println(revStr(str2));
+    }
+
+    public static String revStr(String str)
+    {
+        StringBuilder res = new StringBuilder();
+
+        for (int i=str.length()-1;i>=0;i--)
+        {
+            res.append(str.charAt(i));
+        }
+
+        return res.toString();
+    }
+}
